5f4eb3b8879aa5f0e6ba826d83b8dc36cec0e0ba
Coalery <doralife12@naver.com>
2020-09-07 17:04:50 +0900

    Add: Default load

---
+++ app.js
@@ -0,0 +1,28 @@
+const AWS = require('aws-sdk');
+const express = require('express');
+
+AWS.config.update({
+  region: "ap-northeast-2",
+  endpoint: "https://dynamodb.ap-northeast-2.amazonaws.com"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var table = "Recipe";
+
+var id = "102";
+
+var params = {
+    TableName: table,
+    Key:{
+        "id": id
+    }
+};
+
+docClient.get(params, function(err, data) {
+    if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+    }
+});
