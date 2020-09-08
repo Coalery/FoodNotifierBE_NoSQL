7e925e5b157ba636d49b7d85bcc1deec8dfa8a42
Coalery <doralife12@naver.com>
2020-09-08 17:55:25 +0900

    Add: Apply express

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
 const AWS = require('aws-sdk');
 const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended : false}));
 
 AWS.config.update({
-  region: "ap-northeast-2",
-  endpoint: "https://dynamodb.ap-northeast-2.amazonaws.com"
+  region: 'ap-northeast-2',
+  endpoint: 'https://dynamodb.ap-northeast-2.amazonaws.com'
 });
+
+const RecipeTable = 'Recipe';
+const BarcodeTable = 'Barcode';
+const UserTable = 'User';
+const FoodTable = 'Food';
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-var table = "Recipe";
+app.get('/', (request, response) => {
+    response.send('Hmmm');
+})
 
-var id = "102";
+// ----------------------------------
+//               GET
+// ----------------------------------
 
-var params = {
-    TableName: table,
-    Key:{
-        "id": id
-    }
-};
+// var table = "Recipe";
 
-docClient.get(params, function(err, data) {
-    if (err) {
-        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-    }
-});
+// var id = "102";
+
+// var params = {
+//     TableName: table,
+//     Key:{
+//         "id": id
+//     }
+// };
+
+// docClient.get(params, function(err, data) {
+//     if (err) {
+//         console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+//     } else {
+//         console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+//     }
+// });
